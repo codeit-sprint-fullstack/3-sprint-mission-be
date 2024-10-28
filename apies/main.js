51b77486f453a9f026b54cc6cff26ac6d9f5805d
Jinhan <wlsgks333@naver.com>
2024-10-28 19:24:28 +0900

    Main.js: Product Service, PATCH 메서드 요청 예외 처리 완료.

--- apies/main.js
+++ apies/main.js
@@ -40,7 +40,7 @@ try {
 }
 
 // 상품 등록.
-const surveyData = {
+const surveyDataPost = {
   "name": '배진han', // String
   "description": '저는 노드를 이용해 api를 공부하고 있습니다.', // String
   "price": 999999, // Number
@@ -53,16 +53,40 @@ const surveyData = {
 }
 
 try {
-  // const postSurveyData = await ProductService.createProduct(surveyData)
+  // const postSurveyData = await ProductService.createProduct(surveyDataPost) // surveyDataPost만 있으면 됩니다.
   console.log(postSurveyData)
-} catch (error) {
-  if (error.response) {
-    console.log(error.response.status);
-    console.log(error.response.data);
+} catch (err) {
+  if (err.response) {
+    console.log(err.response.status);
+    console.log(err.response.data);
   } else {
     console.log('리퀘스트가 실패했습니다.');
   }
 }
-// await ProductService.patchProduct()
-// await ProductService.deleteProduct()
 
+// 상품 수정.
+const surveyDataPatch = {
+  "name": '배진han', // String
+  "description": '저는 노드를 이용해 api를 공부하고 있습니다. 지금은 다시 수정하고 있고요 patch를 이용해서요.', // String
+  "price": 999999, // Number
+  "tags": [
+    'tags', '무슨 태그를 달아야 하나' // String
+  ],
+  "images": [
+    'https://helpx.adobe.com/content/dam/help/en/photoshop/using/quick-actions/remove-background-before-qa1.png' // URL
+  ]
+}
+
+try {
+  const patchSurveyDawait = await ProductService.patchProduct(568, surveyDataPatch) // 아규먼트로 id와 surveyDataPatch를 넣어야 합니다.
+  console.log(patchSurveyDawait)
+} catch (err) {
+  if (err.response) {
+    console.log(err.response.status);
+    console.log(err.response.data);
+  } else {
+    console.log('리퀘스트가 실패했습니다.');
+  }
+};
+
+// await ProductService.deleteProduct()
