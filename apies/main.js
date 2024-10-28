888082bf63830f35511fe8a51f0b09f649d6c6d2
Jinhan <wlsgks333@naver.com>
2024-10-28 18:49:51 +0900

    Main.js: Product Service, 상품 상세 조회 호출시 예외 처리 완료

--- apies/main.js
+++ apies/main.js
@@ -13,7 +13,7 @@ import ProductService from './ProductService.js'
 
 // 상품 목록 조회.
 try {
-  const getSurveyData = await ProductService.getProductList()
+  // const getSurveyData = await ProductService.getProductList()
   console.log(getSurveyData)
   
 } catch (err) {
@@ -25,7 +25,20 @@ try {
   }
 }
 
-// await ProductService.getProduct()
+// 상품 상세 조회.
+try {
+  const getSurveyData = await ProductService.getProduct() // 아규먼트로 id 값인 숫자 입력.
+  console.log(getSurveyData)
+  
+} catch (err) {
+  if (err.response) {
+    console.log(err.response.status);
+    console.log(err.response.data);
+  } else {
+    console.log('리퀘스트가 실패했습니다.');
+  }
+}
+
 // await ProductService.createProduct()
 // await ProductService.patchProduct()
 // await ProductService.deleteProduct()
