d7a1a4e19f88329a7a1cc9adb8f70a1252cddff1
Jinhan <wlsgks333@naver.com>
2024-10-28 19:12:37 +0900

    Main: Product Service, 상품 등록 요청 예외 처리 완료

--- apies/main.js
+++ apies/main.js
@@ -27,7 +27,7 @@ try {
 
 // 상품 상세 조회.
 try {
-  const getSurveyData = await ProductService.getProduct() // 아규먼트로 id 값인 숫자 입력.
+  // const getSurveyData = await ProductService.getProduct() // 아규먼트로 id 값인 숫자 입력.
   console.log(getSurveyData)
   
 } catch (err) {
@@ -39,7 +39,30 @@ try {
   }
 }
 
-// await ProductService.createProduct()
+// 상품 등록.
+const surveyData = {
+  "name": '배진han', // String
+  "description": '저는 노드를 이용해 api를 공부하고 있습니다.', // String
+  "price": 999999, // Number
+  "tags": [
+    'tags', '무슨 태그를 달아야 하나' // String
+  ],
+  "images": [
+    'https://helpx.adobe.com/content/dam/help/en/photoshop/using/quick-actions/remove-background-before-qa1.png' // URL
+  ]
+}
+
+try {
+  // const postSurveyData = await ProductService.createProduct(surveyData)
+  console.log(postSurveyData)
+} catch (error) {
+  if (error.response) {
+    console.log(error.response.status);
+    console.log(error.response.data);
+  } else {
+    console.log('리퀘스트가 실패했습니다.');
+  }
+}
 // await ProductService.patchProduct()
 // await ProductService.deleteProduct()
 
