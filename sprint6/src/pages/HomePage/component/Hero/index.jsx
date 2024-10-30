94b41cf4571d14453f9d60d67e8fd9ade12784d4
Jinhan <wlsgks333@naver.com>
2024-10-31 00:05:34 +0900

    Hero 컴포넌트 데스크톱 버전 완료.

--- sprint6/src/pages/HomePage/component/Hero/index.jsx
+++ sprint6/src/pages/HomePage/component/Hero/index.jsx
@@ -5,18 +5,18 @@ import heroImg from '../../../../img/home/hero-image.png';
 
 const Hero = () => {
   return (
-    <section class="skyColorSection">
-      <div class="skyColorContent">
-        <div class="textBox">
-          <div class="text">
-            <div class="text1">일상의 모든 물건을</div>
-            <div class="text1">거래해 보세요</div>
+    <section className="skyColorSection">
+      <div className="skyColorContent">
+        <div className="textBox">
+          <div className="text">
+            <div className="text1">일상의 모든 물건을</div>
+            <div className="text1">거래해 보세요</div>
           </div>
-          <div class="button">
+          <div className="button">
             <a href="/items">구경하러 가기</a>
           </div>
         </div>
-        <div class="imgBox">
+        <div className="imgBox">
           <img src={heroImg} />
         </div>
       </div>
