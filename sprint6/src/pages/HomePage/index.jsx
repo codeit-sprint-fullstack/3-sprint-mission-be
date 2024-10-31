33310a365a3a41d09feedf90b42347b1f79fe4c0
Jinhan <wlsgks333@naver.com>
2024-10-31 09:46:25 +0900

    BottomBanner, Footer 컴포넌트 데스크톱 버전 구현 완료, 태블릿 모바일 버전 X, 우선 순위 변경

--- sprint6/src/pages/HomePage/index.jsx
+++ sprint6/src/pages/HomePage/index.jsx
@@ -4,6 +4,10 @@ import Header from "./component/Header/index.jsx";
 import Hero from "./component/Hero/index.jsx";
 import Section1 from "./component/Section1/index.jsx";
 import Section2 from "./component/Section2/index.jsx";
+import Section3 from "./component/Section3/index.jsx";
+import BottomBanner from "./component/UnderBanner/index.jsx";
+import Footer from "../../components/Footer/index.jsx";
+
 
 function HomePage() {
   return (
@@ -13,10 +17,10 @@ function HomePage() {
         <Hero />
         <Section1 />
         <Section2 />
-      {/*<Section3 />
-      <UnderBanner /> */}
+        <Section3 />
+        <BottomBanner />
       </main>
-      {/*<Footer /> */}
+      <Footer />
     </>
   );
 }
