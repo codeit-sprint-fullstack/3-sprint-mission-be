357e27df0d75bddb352fe894b312f2367063d52e
Yousuk <yousuk88@gmail.com>
2024-11-05 18:04:33 +0900

    add: MongoDB 연결

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); // .env 파일에서 환경 변수 로드
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// JSON 파싱 미들웨어
+app.use(express.json());
+
+// MongoDB 연결
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB 연결됨'))
+  .catch((error) => console.log('MongoDB 연결오류:', error));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
