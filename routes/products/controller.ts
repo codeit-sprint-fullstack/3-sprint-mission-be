1012c65c08b611f294229a1231c55938f0f45769
Daewony <alexnori.com@gmail.com>
2024-11-06 10:28:54 +0900

    refactor: 파일 구조 리팩토링

---
+++ routes/products/controller.ts
@@ -0,0 +1,17 @@
+// service를 정리한 공간
+
+import express from 'express';
+import service from './service';
+
+const router = express.Router();
+
+router.get('/', service.getProductList); // 상품 목록 조회
+router.get(':id', service.getProductDetail); // 상품 상세 조회
+
+router.post('/', service.postProduct); // 상품 등록
+
+router.patch('/:id', service.patchProduct); // 상품 수정
+
+router.delete('/', service.deleteProduct); // 상품 삭제
+
+export default router;
