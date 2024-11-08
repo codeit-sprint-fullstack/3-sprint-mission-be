27c665553cbf709163a437a87f3bf86fa61fa736
ChoboDev <gealot@gmail.com>
2024-11-08 13:46:53 +0900

    🚚 Fix(schema): Update Product schema
    Product 스키마에서 imageUrl의 default값을 없애고 placeholder을 프론트에서 처리하도록 업데이트 하였습니다.

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,7 @@ import mongoose from 'mongoose';
  * @param {Array<string>} tag - Product 태그
  * @param {String} category - Product 카테고리
  * @ ----  아래는 Optional Value ----
- * @param {String} imageUrl - Product 이미지 URL
+ * @param {String} imageUrl - Product 이미지 URL (default 없이 빈 값일 때 프론트에서 placeholder을 설정하도록 결정)
  * @param {String} status - Product 상태(forsale | soldout | hidden)
  * @param {Number} like - Product 좋아요 수
  * @param {Date} createdAt - 생성일은 timestamps로 자동 생성
@@ -40,7 +40,6 @@ const productSchema = new mongoose.Schema(
     },
     imageUrl: {
       type: String,
-      default: `https://placeholderjs.com/220x220&color=_aaa&background=_e7e7e7&text=No+Image`,
     },
     status: {
       type: String,
@@ -55,6 +54,9 @@ const productSchema = new mongoose.Schema(
     timestamps: true,
   },
 );
+
+// imageUrl은 서버에서 default를 설정하기보다 프론트(react)에서 입력받는 값이 없을 때 placeholder 이미지로 설정하는 것이 좋다.
+// placeholder: https://placeholderjs.com/220x220&color=_aaa&background=_e7e7e7&text=No+Image
 
 const Product = mongoose.model('Product', productSchema);
 
