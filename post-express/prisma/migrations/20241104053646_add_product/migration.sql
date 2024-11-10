77915630d31e8d044eb9237676b08f44f7f5cfff
Bluemoon105 <118960498+Bluemoon105@users.noreply.github.com>
2024-11-10 16:11:41 +0900

    sprint7

---
+++ post-express/prisma/migrations/20241104053646_add_product/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "tags" TEXT[],
+    "category" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
