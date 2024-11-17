e3710510ee02c9bbe0e2e8f97b46f54741614932
Jinhan <wlsgks333@naver.com>
2024-11-17 20:30:40 +0900

    Articles, POST, GET 메서드 구현 완료_ 확인 완료

--- sprint7/app.js
+++ sprint7/app.js
@@ -3,13 +3,14 @@ dotenv.config();
 import express from "express";
 import { PrismaClient, Prisma } from "@prisma/client";
 import { assert } from "superstruct";
-import { CreateProduct, PatchProduct } from "./structs.js";
+import { CreateProduct, PatchProduct, CreateArticle } from "./structs.js";
 
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
+// 에러 처리 함수
 function asyncHandler(handler) {
   return async function (req, res) {
     try {
@@ -17,7 +18,7 @@ function asyncHandler(handler) {
     } catch (e) {
       if (
         e.name === 'StructureError' ||
-      e instanceof Prisma.PrismaClientValidationError
+        e instanceof Prisma.PrismaClientValidationError
       ) {
         res.status(400).send({ message: e.message });
       } else if (
@@ -32,6 +33,9 @@ function asyncHandler(handler) {
   }
 }
 
+/** Products Routes **/
+
+// 상품 목록 조회
 app.get('/products', asyncHandler(async (req, res) => {
   const { offset = 0, limit = 10, order = 'newest' } = req.query;
 
@@ -56,7 +60,8 @@ app.get('/products', asyncHandler(async (req, res) => {
   res.send(products);
 }));
 
-app.get('/products/:id', asyncHandler(async (req, res) => {
+// 상품 조회
+app.get('/products/:productId', asyncHandler(async (req, res) => {
   const { id } = req.params;
   const products = await prisma.product.findUniqueOrThrow({
     where: { id },
@@ -68,6 +73,7 @@ app.get('/products/:id', asyncHandler(async (req, res) => {
   }
 }));
 
+// 상품 생성
 app.post('/products', asyncHandler(async (req, res) => {
   assert(req.body, CreateProduct);
   const newProduct = await prisma.product.create({
@@ -77,7 +83,8 @@ app.post('/products', asyncHandler(async (req, res) => {
   res.status(201).send(newProduct);
 }));
 
-app.patch('/products/:id', asyncHandler(async (req, res) => {
+// 상품 수정
+app.patch('/products/:productId', asyncHandler(async (req, res) => {
   const { id } = req.params;
   assert(req.body, PatchProduct);
   const products = await prisma.product.update({
@@ -88,7 +95,8 @@ app.patch('/products/:id', asyncHandler(async (req, res) => {
   res.send(products);
 }))
 
-app.delete('/products/:id', asyncHandler(async (req, res) => {
+// 상품 삭제
+app.delete('/products/:productId', asyncHandler(async (req, res) => {
   const productId = req.params.id;
   const product = await prisma.product.delete({
     where: { id: productId },
@@ -99,5 +107,48 @@ app.delete('/products/:id', asyncHandler(async (req, res) => {
     res.status(404).send({ message: 'id를 확인해주세요.' })
   }
 }))
+
+/** Articles Routes **/
+
+// 게시글 생성
+app.post('/articles', asyncHandler(async (req, res) => {
+  assert(req.body, CreateArticle);
+  const { title, content } = req.body;
+  const nweArticle = await prisma.article.create({
+    data: {
+      title,
+      content,
+    }
+  })
+  console.log(nweArticle);
+  res.status(201).send(nweArticle);
+}))
+
+// 게시글 조회
+app.get('/articles', asyncHandler(async (req, res) => {
+  const { offset = 0, limit = 10, order = 'newest' } = req.query;
+
+  let orderBy;
+  switch (order) {
+    case 'newst':
+      orderBy = { createdAt: 'desc' };
+      break;
+    case 'best':
+      orderBy = { likes: 'desc' };
+      break;
+    case 'oldeest':
+      orderBy = { createdAt: 'asc' };
+      break;
+    default:
+      orderBy = { createdAt: 'desc' };
+  }
+  const articles = await prisma.article.findMany({
+    orderBy,
+    skip: parseInt(offset),
+    take: parseInt(limit),
+  })
+  res.send(articles);
+}))
+
 
 app.listen(process.env.PORT || 8000, () => console.log('Server Started'));
--- sprint7/structs.js
+++ sprint7/structs.js
@@ -1,10 +1,20 @@
 import * as s from 'superstruct';
 
+/** Products Routes **/
+
 export const CreateProduct = s.object({
   name: s.size(s.string(), 1, 10),
   description: s.size(s.string(), 10, 100),
   price: s.number(),
   tags: s.string(),
-})
+});
 
 export const PatchProduct = s.partial(CreateProduct);
+
+/** Articles Routes **/
+
+export const CreateArticle = s.object({
+  title: s.size(s.string(), 1, Infinity),
+  content: s.size(s.string(), 1, Infinity),
+});
+
