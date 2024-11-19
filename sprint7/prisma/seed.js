15e4c504265abb3f67f46ddc3fae7d8b3f3c8f3f
Jinhan <wlsgks333@naver.com>
2024-11-19 17:34:23 +0900

    faker설치 및 사용 해봄.

--- sprint7/prisma/seed.js
+++ sprint7/prisma/seed.js
@@ -2,10 +2,34 @@ import { PrismaClient } from "@prisma/client";
 import { PRODUCTS } from "./mocks/productsMock.js";
 import { ARTICLES } from "./mocks/articlesMock.js";
 import { COMMENTS } from "./mocks/commentsMock.js";
+// import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
 
 async function main() { 
+
+//   const TEST_DATA = [];
+
+//   for (let i = 0; i < 10; i++) {
+//     TEST_DATA.push({
+//       name: faker.commerce.productName(),
+//       avatar: faker.image.avatar(),
+//       description: faker.commerce.productDescription(),
+//       price: faker.commerce.price(),
+//       tags: faker.commerce.productAdjective(),
+
+//       _id: faker.string.uuid(),
+//       avatar: faker.image.avatar(),
+//       birthday: faker.date.birthdate(),
+//       email: faker.internet.email(),
+//       firstName: faker.person.firstName(),
+//       lastName: faker.person.lastName(),
+//       sex: faker.person.sexType(),
+//       subscriptionTier: faker.helpers.arrayElement(['free', 'basic', 'business']),
+//     });
+//   }
+// console.log(TEST_DATA);
+
   // // 기존 데이터 삭제
   // await prisma.product.deleteMany();
   // await prisma.article.deleteMany();
