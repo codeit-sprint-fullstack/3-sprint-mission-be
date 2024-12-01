b3c4ab9bb361e03213d5fd1b52743a3cfbb81770
heonq <gkagjsrb@naver.com>
2024-12-01 20:28:52 +0900

    feat: asyncRequestHandler 수정

--- src/utils/asyncRequestHandler.ts
+++ src/utils/asyncRequestHandler.ts
@@ -1,15 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 import { AsyncRequestHandler } from '../types/asyncRequestHandler';
+import { PrismaClientValidationError } from '@prisma/client/runtime/library';
 
 const asyncRequestHandler = <T>(handler: AsyncRequestHandler<T>) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       await handler(req, res, next);
     } catch (e: any) {
-      if (e.name === 'ValidationError') {
+      if (e.name === 'StructError' || e instanceof PrismaClientValidationError) {
         res.status(400).send({ message: e.message });
-      } else if (e.name === 'CastError') {
-        res.status(404).send({ message: 'Cannot find given id.' });
+      } else if (e.name === 'ValidationError') {
+        res.status(400).send({ message: e.message });
       } else {
         res.status(500).send({ message: e.message });
       }
