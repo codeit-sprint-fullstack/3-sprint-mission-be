39d8be960c7e94eb87a6f1d749bfa81da0e799a6
heonq <gkagjsrb@naver.com>
2024-12-01 20:07:35 +0900

    feat: 서버 실행 코드 수정

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import productRouter from './routes/Products/Controller';
 
 dotenv.config();
-const MONGODB_URI = process.env.DATABASE_URI as string;
 const PORT = process.env.PORT || 8000;
 
 const allowedOrigins = [process.env.DEPLOYED_URL, process.env.LOCALHOST].filter(
@@ -17,19 +15,8 @@ app.use(cors({ origin: allowedOrigins }));
 app.use(express.json());
 app.use('/products', productRouter);
 
-const connectDB = async () => {
+const startServer = () => {
   try {
-    await mongoose.connect(MONGODB_URI);
-    console.log('MongoDB connected');
-  } catch (e) {
-    console.error('MongoDB connection error:', e);
-    process.exit(1);
-  }
-};
-
-const startServer = async () => {
-  try {
-    await connectDB();
     app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
   } catch (e) {
     console.error('Server start error', e);
