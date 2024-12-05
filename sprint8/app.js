1afde8a5a82829d564132f1a38d92222947b21fa
Jinhan <wlsgks333@naver.com>
2024-12-06 01:58:39 +0900

    feat: sprint8 디렉토리 생성 및 sprint7 디렉토리 파일 내용 복사
    -원래 있던 express-sprint6 브랜치 완전히 삭제
    -sprint8 미션에 필요한 백엔드 서버를 배포해야 하기 때문에 브랜치 생성함
    -paginationHandler 함수 추가하고, 코드 Refactoring해서 배포하면 좋을 것
    같음
    -Render에 백엔드 서버와 PostgreSQL 서버를 같이 Project화 해서 같이
    배포할 것

---
+++ sprint8/app.js
@@ -0,0 +1,301 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from "express";
+import { PrismaClient, Prisma } from "@prisma/client";
+import { assert } from "superstruct";
+import {
+  CreateProduct,
+  PatchProduct,
+  CreateArticle,
+  PatchArticle,
+  CreateComment,
+  PatchComment
+} from "./structs.js";
+import orderByFunction from "./orderByFunction.js";
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+
+// 에러 처리 함수
+function asyncHandler(handler) {
+  return async function (req, res) {
+    try {
+      await handler(req, res);
+    } catch (e) {
+      if (
+        e.name === 'StructureError' ||
+        e instanceof Prisma.PrismaClientValidationError
+      ) {
+        res.status(400).send({ message: e.message });
+      } else if (
+        e instanceof Prisma.PrismaClientKnownRequestError &&
+        e.code === 'P2025'
+      ) {
+        res.status(404).send({ message: 'Cannot find given id.' });
+      } else {
+        res.status(500).send({ message: e.message });
+      }
+    }
+  }
+}
+
+/** Products Routes **/
+
+// 상품 목록 조회
+app.get('/products', asyncHandler(async (req, res) => {
+  const { offset = 0, limit = 10, order = 'recent' } = req.query;
+  const offsetNum = parseInt(offset);
+  const limitNum = parseInt(limit);
+
+  const orderBy = orderByFunction(order);
+
+  const products = await prisma.product.findMany({
+    orderBy,
+    skip: offsetNum, // skip으로 offset설정
+    take: limitNum,  // take으로 limit설정
+  });
+  res.send(products);
+}));
+
+// 상품 조회
+app.get('/products/:productId', asyncHandler(async (req, res) => {
+  const { productId } = req.params;
+  const products = await prisma.product.findUniqueOrThrow({
+    where: { id: productId },
+  });
+  if (products) {
+    res.send(products);
+  } else {
+    res.status(404).send({ message: '상품을 찾을 수 없습니다.' })
+  }
+}));
+
+// 상품 생성
+app.post('/products', asyncHandler(async (req, res) => {
+  try {
+    assert(req.body, CreateProduct);
+    const newProduct = await prisma.product.create({
+      data: req.body,
+    })
+    console.log(produnewProductcts);
+    res.status(201).send(newProduct);
+  } catch (error) {
+    res.status(400).send('Bad Request: ' + error.message);
+  }
+}));
+
+// 상품 수정
+app.patch('/products/:productId', asyncHandler(async (req, res) => {
+  const { productId } = req.params;
+  assert(req.body, PatchProduct);
+  const products = await prisma.product.update({
+    where: { id: productId },
+    data: req.body,
+  });
+  console.log(products);
+  res.send(products);
+}))
+
+// 상품 삭제
+app.delete('/products/:productId', asyncHandler(async (req, res) => {
+  const productId = req.params.id;
+  const product = await prisma.product.delete({
+    where: { id: productId },
+  });
+  if (product) {
+    res.sendStatus(204);
+  } else {
+    res.status(404).send({ message: 'id를 확인해주세요.' })
+  }
+}))
+
+/** Articles Routes **/
+
+// 게시글 생성
+app.post('/articles', asyncHandler(async (req, res) => {
+  try {
+    assert(req.body, CreateArticle);
+    const { title, content } = req.body;
+    const nweArticle = await prisma.article.create({
+      data: {
+        title,
+        content,
+      }
+    })
+    console.log(nweArticle);
+    res.status(201).send(nweArticle);
+  } catch (error) {
+    res.status(400).send('Bad Request: ' + error.message);
+  }
+}))
+
+// 게시글 조회
+app.get('/articles/:articleId', asyncHandler(async (req, res) => {
+  const { articleId } = req.params;
+  const articles = await prisma.article.findUniqueOrThrow({
+    where: { id: articleId },
+  });
+  if (articles) {
+    res.send(articles);
+  } else {
+    res.status(404).send({ message: '상품을 찾을 수 없습니다.' })
+  }
+}))
+
+// 게시글 수정
+app.patch('/articles/:articleId', asyncHandler(async (req, res) => {
+  const { articleId } = req.params;
+  assert(req.body, PatchArticle);
+  const articles = await prisma.article.update({
+    where: { id: articleId },
+    data: req.body,
+  });
+  console.log(articles);
+  res.send(articles);
+}))
+
+// 게시글 목록 조회
+app.get('/articles', asyncHandler(async (req, res) => {
+  const { keyword, offset = 0, limit = 10, order = 'recent' } = req.query;
+
+  const orderBy = orderByFunction(order);
+
+  if (keyword) {
+    const articles = await prisma.article.findMany({
+      where: {
+        OR: [
+          { title: { contains: keyword, mode: 'insensitive' } },
+          { content: { contains: keyword, mode: 'insensitive' } },
+        ],
+      },
+      orderBy,
+      skip: parseInt(offset),
+      take: parseInt(limit),
+    })
+    res.send(articles);
+    return;
+  }
+  const articles = await prisma.article.findMany({
+    orderBy,
+    skip: parseInt(offset),
+    take: parseInt(limit),
+  })
+  const totalArticles = await prisma.article.count();
+  const totalPages = Math.ceil(totalArticles / parseInt(limit));
+
+  res.send({ totalArticles, articles, totalPages });
+}))
+
+// 게시글 삭제
+app.delete('/articles/:articleId', asyncHandler(async (req, res) => {
+  const { articleId } = req.params;
+  const article = await prisma.article.delete({
+    where: { id: articleId },
+  });
+  if (article) {
+    res.sendStatus(204);
+  } else {
+    res.status(404).send({ message: 'id를 확인해주세요.' })
+  }
+}))
+
+// 게시글 댓글 목록 조회
+app.get('/articles/:articleId/comments', asyncHandler(async (req, res) => {
+  const { articleId } = req.params;
+  const { cursor, limit = 10, order = 'recent' } = req.query;
+
+  const orderBy = orderByFunction(order);
+  const comments = await prisma.comment.findMany({
+    where: {
+      articleId,
+    },
+    orderBy,
+    take: parseInt(limit),
+    skip: cursor ? 1 : 0,
+    cursor: cursor ? { id: cursor } : undefined,
+  });
+
+  const lastComment = comments[comments.length - 1];
+  const nextCursor = lastComment ? lastComment.id : null;
+
+  res.send({ comments, nextCursor });
+  // send() 메서드를 사용할 때 인자의 값이 복수일 때는 객체로 전달해야 한다.
+}))
+
+/** Comments Routes **/
+
+// 게시글 댓글 등록
+app.post('/articles/:articleId/comments', asyncHandler(async (req, res) => {
+  try {
+    assert(req.body, CreateComment);
+    const { articleId } = req.params;
+    const { content } = req.body;
+    const newComment = await prisma.comment.create({
+      data: {
+        content,
+        article: {
+          connect: { id: articleId },
+        },
+      }
+    })
+    console.log(newComment);
+    res.status(201).send(newComment);
+  } catch (error) {
+    res.status(400).send('Bad Request: ' + error.message);
+  }
+}))
+
+// 게시글 댓글 수정
+app.patch('/comments/:commentId', asyncHandler(async (req, res) => {
+  assert(req.body, PatchComment);
+  const { commentId } = req.params;
+  const { content } = req.body;
+  const comments = await prisma.comment.update({
+    where: { id: commentId },
+    data: { content },
+  });
+  console.log(comments);
+  res.send(comments);
+}))
+
+// 게시글 댓글 삭제
+app.delete('/comments/:commentId', asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const comment = await prisma.comment.delete({
+    where: { id: commentId },
+  });
+  if (comment) {
+    res.sendStatus(204);
+  } else {
+    res.status(404).send({ message: 'id를 확인해주세요.' })
+  }
+}))
+
+// 모드 게시글 댓글 목록 조회
+app.get('/comments', asyncHandler(async (req, res) => {
+  const { cursor, limit = 10, order = 'recent' } = req.query;
+
+  const orderBy = orderByFunction(order);
+  const totalArticleComments = await prisma.comment.count();
+  const comments = await prisma.comment.findMany({
+    orderBy,
+    take: parseInt(limit),
+    skip: cursor ? 1 : parseInt(limit),
+    cursor: cursor ? { id: cursor } : undefined,
+  })
+
+  const lastComment = comments[comments.length - 1];
+  const nextCursor = lastComment ? lastComment.id : null;
+  const totalPage = Math.ceil(totalArticleComments / parseInt(limit));
+
+  res.send({
+    totalArticleComments,
+    comments,
+    nextCursor,
+    totalPage
+  });
+}))
+
+app.listen(process.env.PORT || 8000, () => console.log('Server Started'));
---
+++ sprint8/prisma/migrations/20241117144233_add_comment_product_opptional/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Comment" ALTER COLUMN "productId" DROP NOT NULL;
