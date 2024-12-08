320c29a32f4e752f439cc0db108fff42159c16c7
JeonJun202 <jjg010202@gmail.com>
2024-12-09 01:17:30 +0900

    sprint8 미션 제출

---
+++ mission8-be/prisma/mocks/articleMocks.js
@@ -0,0 +1,22 @@
+export const ArticleMocks = [
+    {
+        title: 'Article 1',
+        content: 'Article 1 content',
+    },
+    {
+        title: 'Article 2',
+        content: 'Article 2 content',
+    },
+    {
+        title: 'Article 3',
+        content: 'Article 3 content',
+    },
+    {
+        title: 'Article 4',
+        content: 'Article 4 content',
+    },
+    {
+        title: 'Article 5',
+        content: 'Article 5 content',
+    },
+];
---
+++ mission8-be/prisma/mocks/productMocks.js
@@ -0,0 +1,32 @@
+export const ProductMocks = [
+    {
+        name: 'Product 1',
+        description: 'Product 1 description',
+        price: 10,
+        tags: ['tag1'],
+    },
+    {
+        name: 'Product 2',
+        description: 'Product 2 description',
+        price: 20,
+        tags: ['tag1', 'tag2'],
+    },
+    {
+        name: 'Product 3',
+        description: 'Product 3 description',
+        price: 30,
+        tags: ['tag3'],
+    },
+    {
+        name: 'Product 4',
+        description: 'Product 4 description',
+        price: 40,
+        tags: [],
+    },
+    {
+        name: 'Product 5',
+        description: 'Product 5 description',
+        price: 50,
+        tags: [],
+    },
+];
