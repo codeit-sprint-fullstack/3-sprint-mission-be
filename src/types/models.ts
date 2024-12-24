49a4879b65a93ecbc9a04c5a671d5e855239d1c9
heonq <gkagjsrb@naver.com>
2024-12-24 17:37:18 +0900

    chore: model type 수정

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,26 +1,51 @@
+export interface userInfo {
+  image: string | null;
+  id: number;
+  nickname: string;
+}
+
 export interface ProductModel {
-  id: string;
+  id: number;
   name: string;
   description: string;
   price: number;
   tags: string[];
+  images: string[];
+  favoriteCount: number;
+  isFavorite: boolean;
+  user: userInfo;
   createdAt: Date;
   updatedAt: Date;
 }
 
 export interface ArticleModel {
-  id: string;
+  id: number;
   title: string;
   content: string;
   createdAt: Date;
   updatedAt: Date;
+  images: string[];
+  user: userInfo;
+  isLiked: boolean;
+  likeCount: number;
 }
 
 export interface CommentModel {
-  id: string;
-  productId: string | null;
-  articleId: string | null;
+  id: number;
+  productId: number | null;
+  articleId: number | null;
   content: string;
+  createdAt: Date;
+  updatedAt: Date;
+  user: userInfo;
+}
+
+export interface UserModel {
+  id: number;
+  email: string;
+  nickname: string;
+  image?: string;
+  encryptedPassword: string;
   createdAt: Date;
   updatedAt: Date;
 }
