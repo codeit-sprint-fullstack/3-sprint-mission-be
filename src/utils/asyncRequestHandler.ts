d6ba2a99328cceb9411e1df8c100ea1363475c30
heonq <gkagjsrb@naver.com>
2024-12-25 17:32:56 +0900

    feat: errorhandler 구현

--- src/utils/asyncRequestHandler.ts
+++ src/utils/asyncRequestHandler.ts
@@ -1,14 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
 import { AsyncRequestHandler } from '../types/asyncRequestHandler';
-import { PrismaClientValidationError } from '@prisma/client/runtime/library';
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from '@prisma/client/runtime/library';
+import { handlePrismaError } from './handlePrismaError';
 
 const asyncRequestHandler = <T>(handler: AsyncRequestHandler<T>) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       await handler(req, res, next);
     } catch (e: any) {
-      if (e.name === 'StructError' || e instanceof PrismaClientValidationError) {
-        res.status(400).send({ message: e.message });
+      if (
+        e.name === 'StructError' ||
+        e instanceof PrismaClientValidationError ||
+        e instanceof PrismaClientKnownRequestError
+      ) {
+        handlePrismaError(e, res);
       } else if (e.name === 'ValidationError') {
         res.status(400).send({ message: e.message });
       } else {
