78799ca6947c8af8b129c430083ff9ce6856d256
heonq <gkagjsrb@naver.com>
2024-12-26 13:24:21 +0900

    feat: auth관련 메시지 추가

--- src/constants/ExceptionMessages.ts
+++ src/constants/ExceptionMessages.ts
@@ -2,4 +2,7 @@ export const EXCEPTION_MESSAGES = {
   articleNotFound: '게시글을 찾을 수 없습니다.',
   productNotFound: '상품을 찾을 수 없습니다.',
   commentNotFound: '댓글을 찾을 수 없습니다.',
+  duplicatedEmail: '이미 사용중인 이메일입니다.',
+  duplicatedNickname: '이미 사용중인 닉네임입니다.',
+  failSignUp: '회원가입에 실패했습니다.',
 };
