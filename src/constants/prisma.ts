3720ea1b8ea30ee9767d30131ce77020915828fc
heonq <gkagjsrb@naver.com>
2024-12-28 22:19:07 +0900

    feat: include,orderBy clause 수정

--- src/constants/prisma.ts
+++ src/constants/prisma.ts
@@ -10,11 +10,24 @@ export const INCLUDE_USER_CLAUSE = {
   },
 } as const;
 
+export const getIncludeFavoriteClause = (userId: number) => {
+  return {
+    favorites: userId
+      ? {
+          where: {
+            userId,
+          },
+        }
+      : false,
+  };
+};
+
 export const getOrderByClause = (orderBy: string) =>
   ({
     recent: { createdAt: Prisma.SortOrder.desc },
     old: { createdAt: Prisma.SortOrder.asc },
     favorite: { favoriteCount: Prisma.SortOrder.desc },
+    like: { likeCount: Prisma.SortOrder.desc },
   })[orderBy];
 
 export const getWhereByWord = (word: string | undefined) => {
