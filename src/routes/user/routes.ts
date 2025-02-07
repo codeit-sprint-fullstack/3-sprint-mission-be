07cf41ec0e177d4f1ea6d8947b5aad00acab9eff
heonq <gkagjsrb@naver.com>
2025-02-07 09:44:30 +0900

    fix: 케이스 수정

--- src/routes/user/routes.ts
+++ src/routes/user/routes.ts
@@ -1,20 +1,14 @@
 import { Router } from 'express';
-import { createAuthMiddleware } from '../../core/middleware/auth/auth';
 import asyncRequestHandler from '../../core/handlers/asyncRequestHandler';
 import { UserService } from './service';
 import { UserController } from './controller';
 import UserRepository from '../../repositories/userRepository';
-import { AUTH_MESSAGES } from '../../constants/authMessages';
 
 const router = Router();
 const userRepository = new UserRepository();
 const userService = new UserService(userRepository);
 const userController = new UserController(userService);
 
-router.get(
-  '/me',
-  // createAuthMiddleware(AUTH_MESSAGES.needLogin),
-  asyncRequestHandler(userController.getMe),
-);
+router.get('/me', asyncRequestHandler(userController.getMe));
 
 export default router;
