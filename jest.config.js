661d614c9314d01f2300ad76fbeba76ffc3fbb79
heonq <gkagjsrb@naver.com>
2025-02-15 12:53:08 +0900

    chore: jest관련 설정

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest'],
+  },
+};
