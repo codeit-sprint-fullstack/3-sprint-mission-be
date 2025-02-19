48ec6c4199fc726584a781824b3de5e11b6438c0
Daewon Kang <alexnori.com@gmail.com>
2025-02-19 17:31:27 +0900

    feat: 댓글 수정 API 구현

--- nestjs/src/comments/comments.controller.ts
+++ nestjs/src/comments/comments.controller.ts
@@ -6,37 +6,35 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Request,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { ApiOperation } from '@nestjs/swagger';
+import { PassportJwtAuthGuard } from 'src/auth/guards/passport-jwt.guard';
 
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
-  @Post()
-  create(@Body() createCommentDto: CreateCommentDto) {
-    return this.commentsService.create(createCommentDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.commentsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.commentsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
-    return this.commentsService.update(+id, updateCommentDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.commentsService.remove(+id);
+  // 댓글 수정
+  @Patch(':commentId')
+  @UseGuards(PassportJwtAuthGuard)
+  @ApiOperation({ summary: '댓글 수정' })
+  updateComment(
+    @Param('commentId') commentId: string,
+    @Request() request: { user?: { userId: string } },
+    @Body() { content }: { content: string },
+  ) {
+    if (!request.user) {
+      throw new UnauthorizedException(
+        '상품 수정하기 위해 로그인이 필요합니다.',
+      );
+    }
+    const userId = request.user.userId;
+    return this.commentsService.updateComment(commentId, userId, content);
   }
 }
--- nestjs/src/comments/comments.service.ts
+++ nestjs/src/comments/comments.service.ts
@@ -1,26 +1,60 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class CommentsService {
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
-  }
+  constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
-    return `This action returns all comments`;
-  }
+  async updateComment(commentId: string, userId: string, content: string) {
+    // 해당 댓글이 존재하는지 확인
+    const comment = await this.prisma.comment.findUnique({
+      where: {
+        id: commentId,
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            nickname: true,
+            profile_image: true,
+          },
+        },
+      },
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
-  }
+    if (!comment) {
+      throw new Error('댓글이 존재하지 않습니다.');
+    }
 
-  update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
-  }
+    // 해당 댓글의 작성자랑 맞는지 확인
+    if (comment.user.id !== userId) {
+      throw new Error('댓글 작성자만 수정할 수 있습니다.');
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} comment`;
+    // 댓글 수정
+    const updatedComment = await this.prisma.comment.update({
+      where: { id: commentId },
+      data: { content },
+    });
+
+    console.log('updatedAt', updatedComment.updatedAt);
+    console.log(
+      'updatedAt.toISOString',
+      updatedComment.updatedAt.toISOString(),
+    );
+
+    return {
+      writer: {
+        image: comment.user.profile_image,
+        nickname: comment.user.nickname,
+        id: comment.user.id,
+      },
+      updatedAt: updatedComment.updatedAt.toISOString(),
+      createdAt: updatedComment.createdAt.toISOString(),
+      content: updatedComment.content,
+      id: updatedComment.id,
+    };
   }
 }
