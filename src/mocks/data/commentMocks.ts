39dd30015ab98366bc27787c929a4ffaf100715f
heonq <gkagjsrb@naver.com>
2025-02-22 16:57:20 +0900

    chore: commentMockData에 id값 추가

--- src/mocks/data/commentMocks.ts
+++ src/mocks/data/commentMocks.ts
@@ -1,4 +1,5 @@
 export const productCommentMocks = Array.from({ length: 10 }, (_, i) => ({
+  id: i + 1,
   content: `댓글${i + 1}`,
   articleId: null,
   productId: i + 1,
@@ -6,6 +7,7 @@ export const productCommentMocks = Array.from({ length: 10 }, (_, i) => ({
 }));
 
 export const articleCommentMocks = Array.from({ length: 10 }, (_, i) => ({
+  id: i + 1,
   content: `댓글${i + 1}`,
   articleId: i + 1,
   productId: null,
